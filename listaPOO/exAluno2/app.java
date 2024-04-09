fb3a8762c5bb021512ad662d9a490a6045ea1a71
João Victor Carvalho <142843652+joaokrv@users.noreply.github.com>
2024-04-09 13:53:09 -0300

    Add files via upload

---
+++ listaPOO/exAluno2/app.java
@@ -0,0 +1,16 @@
+package exAluno2;
+
+public class app {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Hello, World!");
+
+        aluno aluno1 = new aluno("João", 123456, 25, 30, 35);
+        aluno aluno2 = new aluno("Maria", 654321, 20, 30, 40);
+
+        System.out.println(aluno1.apresentar());
+        System.out.println("\n" + aluno2.apresentar());
+
+    }
+}
+//
